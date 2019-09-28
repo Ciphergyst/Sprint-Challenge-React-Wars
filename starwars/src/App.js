fcb016ad61e5e192018584130eeca482893727a0
Ciphergyst <Jeremy McGrath>
2019-09-28 11:45:04 -0700

    fully functional, but not yet styled

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,14 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Starwarslist from"./components/Starwarslist.js"
+import Pagination from "./components/Pagination"
 import axios from "axios"
 
 const App = () => {
   const [starwars, setStarwars] = useState([])
+  const [currentPageUrl, setCurrentPageUrl]= useState("https://swapi.co/api/people/")
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [prevPageUrl, setPrevPageUrl] = useState()
+  const [loading, setLoading] = useState(true)
 
-  axios.get("https://swapi.co/api/people/").then(res => {
+  useEffect(() => {
+    setLoading(true)
+    let cancel
+    axios.get(currentPageUrl, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
+      setLoading(false)
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
     setStarwars(res.data.results.map(s => s.name))
-  })
+   })
+
+   return () => cancel() 
+  }, [currentPageUrl])
+
+  function gotoNextPage() {
+    setCurrentPageUrl(nextPageUrl)
+  }
+
+  function gotoPrevPage() {
+    setCurrentPageUrl(prevPageUrl)
+  }
+
+  if (loading) return "Loading..."
+  
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -19,7 +46,13 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      <Starwarslist starwars={starwars} />
+      <>
+        <Starwarslist starwars={starwars} />
+        <Pagination 
+          gotoNextPage={nextPageUrl ? gotoNextPage : null}
+          gotoPrevPage={prevPageUrl ? gotoPrevPage : null}
+        />
+      </>
     </div>
    
    
