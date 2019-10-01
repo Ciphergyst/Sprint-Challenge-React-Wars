96d275b003857475d0601d108a22bacf2176aed3
Ciphergyst <Jeremy McGrath>
2019-10-01 01:11:50 -0700

    Im so darn close

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import './App.css';
+import Starwars from "./components/starwars";
+// import CardCreator from "./components/CardCreator"
+import Pagination from "./components/Pagination.js"
+// import Starwarslist from"./components/Starwarslist.js"
+
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [starwars, setStarwars] = useState([])
+  const [currentPageUrl, setCurrentPageUrl]= useState("https://swapi.co/api/people/")
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [prevPageUrl, setPrevPageUrl] = useState()
+  const [loading, setLoading] = useState(true)
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  useEffect(() => {
+    setLoading(true)
+    let cancel
+    axios.get(currentPageUrl, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
+      setLoading(false)
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
+      setStarwars(res.data.results)
+   })
+
+   return () => cancel() 
+  }, [currentPageUrl])
+
+  function gotoNextPage() {
+    setCurrentPageUrl(nextPageUrl)
+  }
+
+  function gotoPrevPage() {
+    setCurrentPageUrl(prevPageUrl)
+  }
+
+  if (loading) return "Loading..."
 
   return (
-    <div className="App">
+    <div className="Container">
       <h1 className="Header">React Wars</h1>
+      
+      <>
+       
+         {starwars.map(s => {
+           return <Starwars s={s} key={s.name} />
+         })}
+       
+        
+        <Pagination 
+           gotoNextPage={nextPageUrl ? gotoNextPage : null}
+           gotoPrevPage={prevPageUrl ? gotoPrevPage : null}
+        />
+      </>
+
     </div>
   );
 }
---
+++ starwars/src/components/CardCreator.js
@@ -0,0 +1,27 @@
+// import React, { useEffect, useState } from "react";
+// import axios from "axios";
+// import Characters from "./components/Characters"
+// import App from "./App.js"
+// import { Container, Grid } from "semantic-ui-react"
+
+
+//  function CardCreator({ limit }) {
+//   const [cardInfo, setCardInfo] = useState([]);
+
+
+//   useEffect(() => {
+//     axios
+//     .get(`https://swapi.co/api/people/`, {
+//       params: {}
+//     }
+//   )
+//       .then(res => {
+//         const results = res.data.results
+//         console.log(results)
+//         setCardInfo(results)
+//       });
+//   }, []);
+
+  
+
+//   export default CardCreator
